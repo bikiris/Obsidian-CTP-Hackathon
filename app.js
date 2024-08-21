140099db72142c0d1c66127e67a1b25f668a7dc0
fedy <fedy.cherif26@cuny.edu>
2024-08-21 12:39:01 -0400

    Modifications

--- app.js
+++ app.js
@@ -16,13 +16,13 @@ var server = require("./server/server.js");
 // gets the server
 
 var app = express();
-
 app.set('port', process.env.PORT || 3000);
-//app.use(favicon(__dirname + '/public/images/favicon.png'));
-//app.use(logger('dev'));
-//app.use(bodyParser.urlencoded({ extended: false }));
-//app.use(methodOverride('_method'));
+app.use(favicon(__dirname + '/public/images/favicon.png'));
+app.use(logger('dev'));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(methodOverride('_method'));
 app.use(express.static(path.join(__dirname, 'client/build')));
+app.use(express.json());
 
 if (app.get('env') == 'development') {
   app.locals.pretty = true;
@@ -30,9 +30,10 @@ if (app.get('env') == 'development') {
 
 // The "catchall" handler: for any request that doesn't
 // match one above, send back React's index.html file.
-/*app.get('/home', (req, res) => {
+app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname+'/client/build/index.html'));
-});*/
+});
+
 
 server.useApp(app);
 
@@ -40,12 +41,12 @@ server.useApp(app);
 // For the backend if you want to add more go for it
 
 // Defining a route for handling client communication
-app.get('/api/message', (req, res) => {
+/*app.get('/api/message', (req, res) => {
   
   server.getMessage(req, res);
   
-});
-
+});*/
+/**/
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log("Express server listening on port " + app.get('port'));
