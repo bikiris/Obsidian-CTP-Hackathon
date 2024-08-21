b7e588e175cf8a7ee7038757c0af29f7573dea7b
fedy <fedy.cherif26@cuny.edu>
2024-08-21 14:40:08 -0400

    Restarted the whole project because it was severly broken

---
+++ public/main.js
@@ -0,0 +1,7 @@
+// main.js
+
+async function getMessage() {
+  const response = await fetch('/api/message');
+  const data = await response.json();
+  document.getElementById('message').innerText = data.message;
+}
